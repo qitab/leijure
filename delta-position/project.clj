c22cf0cefe5d3b81537bdc14fb1dd0b9089ebacf
Francois-Rene Rideau <tunes@google.com>
2014-07-04 01:23:27 -0400

    Separate code and test in separate directories for delta-position.

--- delta-position/project.clj
+++ delta-position/project.clj
@@ -3,4 +3,8 @@
   :url "TBD"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :main "leijure.delta-position"
+  :jar-name "leijure.delta-position.jar")
